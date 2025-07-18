9b6bab2156c1e9dab68bd296fe6548bf7ffa83e4
student-Imran <ahmedimranmd55@gmail.com>
2025-07-18 21:19:48 +0600

    loj-problem

--- Practice/loj_Commandos.cpp
+++ Practice/loj_Commandos.cpp
@@ -1,56 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1e5+9;
-vector<int>g[N];
-bool vis[N];
-int dis[N];
-int par[N];
+int test;
 void solve()
 {
+   test++;
+   vector<int>dis(110,0);
+   vector<bool>vis(110,false);
+   vector<int>par(110,0);
+   vector<int> g[110];
    int n,m;cin>>n>>m;
-   for(int i=0;i<n;i++){
-      par[i]=-1;
-   }
    while(m--){
    	int u,v;cin>>u>>v;
    	g[u].push_back(v);
-      g[v].push_back(u);
+   	g[v].push_back(u);
    }
-   //cout<<par[3]<<'\n';
-   int st,meet;cin>>st>>meet;
+   int start,dest;
+   cin>>start>>dest;
    queue<int>q;
-   q.push(st);
-   vis[st]=true;
-   dis[st]=0;
+   q.push(start);
+   vis[start]=true;
+   dis[start]=0;
+   //Bfs
    while(!q.empty()){
-   	int f=q.front();
-   	q.pop();
-
-   	for(auto x:g[f]){
-       if(!vis[x]){
-       	q.push(x);
-         par[x]=f;
-       	dis[x]=dis[f]+1;
-       	vis[x]=true;
-         
-       }
-   	}
-     
-   }
-   int ans=0;
-   for(int i=0;i<n;i++){
-      if(par[i]==st){
-         ans+=dis[i];
+   int u=q.front();
+   q.pop();
+   for(auto x : g[u]){
+      if(!vis[x]){
+      	q.push(x);
+      	par[x]=u;
+      	dis[x]=dis[u]+1;
+      	vis[x]=true;
       }
-      //cout<<dis[i]<<" ";
    }
-   for(int i=0;i<n;i++){
-      cout<<par[i]<<" ";
-   }
+  }
+  int ans=-1;
+  for (int i = 0; i <n; ++i)
+  {
+     queue<int>q1;
+     q1.push(i);
+     vector<int>dis1(110,0);
+     vector<bool>vis1(110,false);
+     vis1[i]=true;
+     dis1[i]=0;
+     while(!q1.empty()){
+     int u=q1.front();
+     q1.pop();
+     for(auto x : g[u]){
+      if(!vis1[x]){
+         q1.push(x);
+         dis1[x]=dis1[u]+1;
+         vis1[x]=true;
+      }
+     }
+  }
+  ans=max(ans,(dis[i]+dis1[dest]));
 
-   
-  
-
+  }cout<<"Case "<<test<<": "<<ans<<'\n';
 }
 int main()
 {
@@ -58,7 +63,7 @@ int main()
 	cin.tie(NULL);
 
 
-	int t=1;
+	int t;
 	cin>>t;
 	while(t--)
 	{
